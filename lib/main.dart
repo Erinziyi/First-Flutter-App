e2733949216f2e02c5b4768cf09f177fdccd5f64
unknown <erin@peoplelogy.com>
2019-01-30 18:25:35 +0800

    Done List View Scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,15 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random(); // Add this line
     return new MaterialApp(
       title: 'Startup Name Generator',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new Scaffold(
-        appBar : new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-
-           body: new Center( // Change "const" to "new".
-             //child: const Text('Hello World'), // Replace this text
-             //child: new Text(wordPair.asPascalCase), // with this text
-             child: new RandomWords(),
-           ),
-        ),
+      home: new RandomWords(),
     );
   }
 }
+
 
 class RandomWords extends StatefulWidget {
   @override
@@ -43,15 +20,15 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    //final WordPair wordPair = new WordPair.random();
-    final List<WordPair>_suggestions = <WordPair>[];
-    final TextStyle_biggerFont = const TextStyle(fontSize: 18.0);
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Startup Name Generator'),
-        ),
+      appBar: new AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
       body: _buildSuggestions(),
     );
   }
@@ -59,35 +36,16 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return new ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        // The itemBuilder callback is called once per suggested
-        // word pairing, and places each suggestion into a ListTile
-        // row. For even rows, the function adds a ListTile row for
-        // the word pairing. For odd rows, the function adds a
-        // Divider widget to visually separate the entries. Note that
-        // the divider may be difficult to see on smaller devices.
         itemBuilder: (BuildContext _context, int i) {
-          // Add a one-pixel-high divider widget before each row
-          // in the ListView.
           if (i.isOdd) {
-            return new Divider();
+            return const Divider();
           }
-
-          // The syntax "i ~/ 2" divides i by 2 and returns an
-          // integer result.
-          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-          // This calculates the actual number of word pairings
-          // in the ListView,minus the divider widgets.
           final int index = i ~/ 2;
-          // If you've reached the end of the available word
-          // pairings...
           if (index >= _suggestions.length) {
-            // ...then generate 10 more and add them to the
-            // suggestions list.
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
-        }
-    );
+        });
   }
 
   Widget _buildRow(WordPair pair) {
@@ -98,10 +56,11 @@ class RandomWordsState extends State<RandomWords> {
       ),
     );
   }
-
-
-
 }
+
+
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
